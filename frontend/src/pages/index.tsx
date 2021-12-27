66772d1c85a70ad301a01f422ae866d176b2ed36
clobbe <clobbe.norman@gmail.com>
2021-12-27 16:59:35 +0100

    ADD :: adding initial project structure + initial scope/todo to each page

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React, { VFC } from "react";
+import { Container, Row, Col } from "react-grid-system";
+import styled from "styled-components";
+
+/*
+
+    TODO :: svg icons for "are you intro / extro"
+    TODO :: add introductionary text
+    TODO :: add "get started"-button
+    TODO :: create button-component to reuse
+    TODO :: create radiobutton-component to reuse between pages
+    TODO :: create paper-component to frame the content
+
+ */
+
+const Index: VFC = () => {};
+
+export default Index;
---
+++ frontend/src/pages/result.tsx
@@ -0,0 +1,16 @@
+import React, { VFC } from "react";
+import { Container, Row, Col } from "react-grid-system";
+import styled from "styled-components";
+
+/* 
+    SCOPE :: calculate the score and show if person is intro / extrovert
+    TODO :: implement function to calculate score
+    TODO :: implement fetching of individual question-scores
+    TODO :: create components needed in order to display result
+    TODO :: add some svg-img for intro/extrovert + an animation in a circle around the svg. i.e. "You must be {intro/extrovert}"
+    TODO :: display the score
+*/
+
+const Results: VFC = () => {};
+
+export default Results;
