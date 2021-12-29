45af60ad63e5dc967ba0ec685ae10cf838e342d9
clobbe <clobbe.norman@gmail.com>
2021-12-29 16:56:22 +0100

    ADD :: adding introductionary text on Index-page

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col } from "react-grid-system";
+import { Link } from "react-router-dom";
 import Button from "../components/atoms/Button";
 import Card from "../components/atoms/Card";
 
@@ -18,11 +19,23 @@ const Index = () => {
     <Container>
       <Card>
         <Row>
-          <Col>Index Page</Col>
+          <Col>
+            <p>
+              With this app you get to answer 5 questions that will tell if your
+              most likely introverted or extroverted kind of person. Or perhaps
+              somewhere in between 😉 <br /> Good luck and have fun!
+            </p>
+            <p>
+              <b>Tip!</b> on each question, pick the answer that first align
+              most with your beliefs and gut feeling.
+            </p>
+          </Col>
         </Row>
         <Row>
           <Col>
-            <Button>Next</Button>
+            <Link to="/questionaire">
+              <Button>Get Started</Button>
+            </Link>
           </Col>
         </Row>
       </Card>
