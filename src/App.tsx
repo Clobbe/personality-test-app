2a35e4d49a05cbf591c3ab0f6b841028c8906e8e
Clobbe <clobbe.norman@gmail.com>
2022-06-02 15:15:17 +0200

    FIX : upgrading node_modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Container, Row, Col } from "react-grid-system";
 import Index from "./pages/Index";
 import Questionaire from "./pages/Questionaire";
 import Result from "./pages/Result";
+import { VFC } from "react";
 
-function App() {
+const App: VFC = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Personality Test App</h1>
-          <h3>Are you 🤩 (extrovert) or 😶 (introvert)</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Switch>
-            <Route exact path="/">
-              <Index />
-            </Route>
-            <Route path="/questionaire">
-              <Questionaire />
-            </Route>
-            <Route path="/result">
-              <Result />
-            </Route>
-          </Switch>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Personality Test App</h1>
+            <h3>Are you 🤩 (extrovert) or 😶 (introvert)</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Switch>
+              <Route exact path="/">
+                <Index />
+              </Route>
+              <Route path="/questionaire">
+                <Questionaire />
+              </Route>
+              <Route path="/result">
+                <Result />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
-}
+};
 
 export default App;
