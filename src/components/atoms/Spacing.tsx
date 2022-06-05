f11075fb439e90cc04705fb52b3c8ae6bf5e7dd9
Clobbe <clobbe.norman@gmail.com>
2022-06-05 16:15:19 +0200

    CU-2w4h1fn :: Adding spacing component

---
+++ src/components/atoms/Spacing.tsx
@@ -0,0 +1,22 @@
+import React, { VFC } from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props extends React.HTMLProps<HTMLDivElement> {
+  spacing?: number;
+  horizontal?: boolean;
+}
+const StyledSpacer = styled.div<Props>`
+  height: ${({ spacing }) => spacing && spacing * 8}px;
+  ${({ horizontal, spacing }) =>
+    horizontal &&
+    css`
+      width: ${spacing && spacing * 8}px;
+      height: 0px;
+    `};
+`;
+
+const Spacing: VFC<Props> = ({ horizontal, spacing = 1 }) => (
+  <StyledSpacer horizontal={horizontal} spacing={spacing} />
+);
+
+export default Spacing;
